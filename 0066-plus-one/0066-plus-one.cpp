ab7e741aebac912eb655bc01ef563bcb050adb24
Pragnyayelisetti <Pragnyayelisetti@gmail.com>
2026-01-01 11:16:03 +0530

    Time: 0 ms (100%), Space: 11.3 MB (96.43%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
+        int c=0,s;
         vector<int>arr;
-        int sum=0,c=0;
         for(int i=digits.size()-1; i>=0; i--){
-            if(i==digits.size()-1) sum=digits[i]+1+c;
-            else sum=digits[i]+c;
-            c=sum/10;//1
-            if(c==0){
-                arr.push_back(sum);
-                sum=0;
-            }
-            else{
-                arr.push_back(sum%10);
-                sum=c;
-            }
+            if(i==digits.size()-1) s=digits[i]+1+c;
+            else s=digits[i]+c;
+            arr.push_back(s%10);
+            c=s/10;
         }
         if(c!=0) arr.push_back(c);
         reverse(arr.begin() , arr.end());
