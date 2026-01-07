036bcb7c6711b8b860f30d372c9e76647836d732
Pragnyayelisetti <Pragnyayelisetti@gmail.com>
2026-01-07 06:39:42 +0530

    Time: 0 ms (100%), Space: 11.9 MB (63.79%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int f=0;
+        string str;
+        for(int i=0; i<strs[0].size(); i++){
+            char ch=strs[0][i];
+            for(int j=1; j<strs.size(); j++){
+                char c=strs[j][i];
+                if(i>=strs[j].size() || ch!=c){
+                    f=1;
+                    break;
+                }
+            }
+            if(f==0) str.push_back(ch);
+            else break;
+        }
+        return str;
+    }
+};
