a45e62f74724bb024d278c44d7dedc9b7b83d5e8
Pragnyayelisetti <Pragnyayelisetti@gmail.com>
2026-01-25 18:10:57 +0530

    Time: 0 ms (100%), Space: 193.5 MB (54.42%) - LeetHub

---
+++ 3818-minimum-prefix-removal-to-make-array-strictly-increasing/3818-minimum-prefix-removal-to-make-array-strictly-increasing.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minimumPrefixLength(vector<int>& nums) {
+        for(int i=nums.size()-1; i>0; i--){
+            if(nums[i]>nums[i-1]) continue;
+            else return i;
+        }
+        return 0;
+    }
+};
