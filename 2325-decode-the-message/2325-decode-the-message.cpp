ed6b22d11e651706bc5e9328589e5226b844a853
Pragnyayelisetti <Pragnyayelisetti@gmail.com>
2026-05-26 21:24:22 +0530

    Time: 12 ms (4.81%), Space: 9.9 MB (29.21%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        unordered_map<char,char>freq;
+        char ch='a';
+        for(int s:key){
+            if(s!=' ' && freq.find(s)==freq.end()){
+                freq[s]=ch;
+                ch++;
+            }
+        }
+        for(auto it:freq) cout<<it.first<<" "<<it.second<<endl;
+        string res="";
+        for(char ch:message){
+            if(ch==' ') res+=' ';
+            else res+=freq[ch];
+        }
+        return res;
+    }
+};
