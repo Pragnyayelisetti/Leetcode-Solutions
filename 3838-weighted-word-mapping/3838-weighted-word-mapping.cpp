fa0947f0688f95a21e88304583d14f32f9420347
Pragnyayelisetti <Pragnyayelisetti@gmail.com>
2026-06-13 20:31:01 +0530

    Time: 0 ms (100%), Space: 43.6 MB (77.6%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string s;
+        for(int i=0; i<words.size(); i++){
+            int sum=0;
+            for(int j=0; j<words[i].size(); j++){
+                char ch=words[i][j];
+                sum+=weights[ch-'a'];
+            }
+            sum=sum%26;
+            char ch='a'+26-sum-1;
+            s+=ch;
+        }
+        return s;
+    }
+};
