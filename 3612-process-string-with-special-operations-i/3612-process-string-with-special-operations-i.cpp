d0c55b213d009668603324a4919a1a3b753b02e6
Pragnyayelisetti <Pragnyayelisetti@gmail.com>
2026-06-16 16:45:50 +0530

    Time: 5 ms (45.83%), Space: 39.7 MB (87.5%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string res;
+        for(char ch : s){
+            if(ch>='a' && ch<='z'){
+                res += ch;
+            }
+            else if(ch=='*' && !res.empty()){
+                res.pop_back();
+            }
+            else if(ch=='#'){
+                res = res + res;
+            }
+            else if(ch=='%'){
+                reverse(res.begin(),res.end());
+            }
+        }
+        return res;
+    }
+};
