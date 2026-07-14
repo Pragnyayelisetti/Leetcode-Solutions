0319f362799df85d26fa078dca93cd02b3f2b04d
Pragnyayelisetti <Pragnyayelisetti@gmail.com>
2026-07-14 22:10:39 +0530

    Time: 0 ms (100%), Space: 17.3 MB (32.7%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,44 +1,33 @@
 class Solution {
 public:
     vector<int> findBall(vector<vector<int>>& grid) {
-        int i,j;
+        int i=0,j=0;
         int n=0;
         vector<int>ans;
-        while(n<grid[0].size()){
-            i=0,j=n;
-            while(i<grid.size()){
-                if(grid[i][j]==1 && j+1<grid[i].size() && grid[i][j+1]==1){
-                    if(i==grid.size()-1){
-                        ans.push_back(j+1);
-                        break;
-                    }
-                    else {
-                        i++;
-                        j++;
-                    }
-
+        // if(grid.size()==1){
+        //     ans.push_back(-1);
+        //     return ans;
+        // }
+        while(n<grid[i].size()){
+            while(i<grid.size() && j<grid[i].size()){
+                if(i<grid.size()-1 && grid[i][j]==1 && j+1<grid[i].size() && grid[i][j+1]==1){
+                    i++;
+                    j++;
                 }
-                else if(grid[i][j]==-1 && j-1>=0 && grid[i][j-1]==-1){
-                    if(i==grid.size()-1){
-                        ans.push_back(j-1);
-                        break;
-                    }
-                    else {
-                        i++;
-                        j--;
-                    }
+                else if(i<grid.size()-1 && grid[i][j]==-1 && j-1>=0 && grid[i][j-1]==-1){
+                    i++;
+                    j--;
                 }
                 else{
-                    ans.push_back(-1);
                     break;
                 }
             }
-            // if(grid[i][j]==-1 && i==grid.size()-1) ans.push_back(j-1);
-            // else if(grid[i][j]==1 && i==grid.size()-1) ans.push_back(j);
-            // else ans.push_back(-1);
-           // i=0;
+            if(grid[i][j]==-1 && i==grid.size()-1 && j-1>=0 && grid[i][j-1] ==-1) ans.push_back(j-1);
+            else if(grid[i][j]==1 && i==grid.size()-1 && j+1<grid[i].size() && grid[i][j+1]==1) ans.push_back(j+1);
+            else ans.push_back(-1);
+            i=0;
             n++;
-            //j=n;
+            j=n;
         }
         return ans;
     }
