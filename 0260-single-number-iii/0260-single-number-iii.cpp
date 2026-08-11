b7056a4b6745440ccf2e3df379380cc2a176dbc2
Pragnya Yelisetti <Pragnyayelisetti@gmail.com>
2026-08-11 11:55:57 +0530

    Time: 0 ms (100%), Space: 14 MB (56.86%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         int xr=0;
-        for(int num:nums) xr=xr^num;
-        long long n=(long long)xr&-(long long)xr;
+        for(int num:nums) xr=xr^num;//xor of all numbers
+        long long n=(long long)xr&-(long long)xr;//gets the rightmost set bit
         int x=0,y=0;
+        //dividing into groups based on the rightmost set bit
         for(int num:nums){
             if(num&n) x=x^num;
             else y=y^num;
