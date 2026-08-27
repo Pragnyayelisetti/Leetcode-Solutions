ba6d28a097cbb6b5d669aba65a6b5be0921ed0ef
Pragnya Yelisetti <Pragnyayelisetti@gmail.com>
2026-08-27 07:03:17 +0530

    Time: 2 ms (77.18%), Space: 7.8 MB (38.54%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    vector<int>dp=vector<int>(31 , -1);
+    vector<int>dp=vector<int>(31, -1);
     int fib(int n) {
-        if(dp[n]!=-1) return dp[n];
         if(n==0) return 0;
-        else if(n==1) return 1;
+        if(n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
         return dp[n]=fib(n-1)+fib(n-2);
     }
 };
